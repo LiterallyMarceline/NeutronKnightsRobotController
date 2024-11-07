a3dd3b572683ce8900bc1f55aa74c6cd9cc45189
TanushFTC <tanush.moolchandani@masonrobotics.org>
2024-11-06 19:46:59 -0500

    New Edits - BeltedViperSlideKit was added, and so was the spinning intake. The servo teleop was created, and will be edited later on.

--- TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/opmodes/ServoTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/opmodes/ServoTeleOp.java
@@ -3,41 +3,72 @@ package org.firstinspires.ftc.neutronknightscode.main.robot.opmodes;
 import static org.firstinspires.ftc.neutronknightscode.main.robot.Robot.servo;
 
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class ServoTeleOp extends RobotTeleOp {
+
+    private CRServo IntakeServo;
+    private CRServo IntakeServo2;
+    private CRServo BaseServo;
+    private CRServo PivotServo;
+
+    /*private DcMotor WonkyServo; */
+
+    private DcMotor WonkyServo;
+
+    public void init() {
+        /* WonkyServo = hardwareMap.get(DcMotor.class, "WonkyServo"); */
+
+        IntakeServo = hardwareMap.get(CRServo.class, "IntakeServo4");
+        IntakeServo2 = hardwareMap.get(CRServo.class, "IntakeServo5");
+        BaseServo = hardwareMap.get(CRServo.class, "BaseServo2");
+        PivotServo = hardwareMap.get(CRServo.class, "PivotServo3");
+
+    }
+
     @Override
-    public void loop(){
+    public void loop() {
         super.loop();
-        long x = 250;
-        double w = 1;
-        double y = 0;
-        double z = 180;
 
-        if (gamepad1.right_bumper) {
+        double Power = gamepad2.left_stick_y;
 
+        /* 6 Revolutions!!! */
 
-            servo.setPower(1);
+        /* if (gamepad1.b) {
+            double x = (double) 180 /312;
+
+            WonkyServo.setPower(-x);
 
             try {
-                Thread.sleep(x);
+                Thread.sleep(5500);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
+
+            WonkyServo.setPower(0);
         }
+        if (gamepad1.a) {
+            double x = (double) 180 /312;
 
-        if (gamepad1.left_bumper) {
-
-
-            servo.setPower(-1);
+            WonkyServo.setPower(x);
 
             try {
-                Thread.sleep(x);
+                Thread.sleep(5500);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
-        }
 
-        double Power = gamepad1.left_stick_y;
+            WonkyServo.setPower(0);
+        }
+        */
+
+        IntakeServo.setPower(-Power);
+        IntakeServo2.setPower(Power);
+
+
+        BaseServo.setPower(gamepad2.right_trigger);
+        PivotServo.setPower(gamepad2.left_trigger);
     }
 }
