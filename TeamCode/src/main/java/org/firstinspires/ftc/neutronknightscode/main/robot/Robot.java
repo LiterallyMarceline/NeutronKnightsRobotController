ca870f64b3fed52a23a4e79e96b363e0894cac54
TanushFTC <tanush.moolchandani@masonrobotics.org>
2024-11-07 19:41:20 -0500

    Working Code 1.0

--- TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/Robot.java
@@ -4,13 +4,11 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.neutronknightscode.main.robot.mechanisms.Drivetrain;
 import org.firstinspires.ftc.neutronknightscode.main.robot.mechanisms.Servo;
-import org.firstinspires.ftc.neutronknightscode.main.robot.mechanisms.SpinningBrushIntake;
 
 public class Robot {
 
     public static Drivetrain drivetrain;
-    public static SpinningBrushIntake spinningBrushIntake;
-    public static Servo servo;
+    //public static Servo servo;
 
     private Robot(){
         drivetrain = new Drivetrain();
@@ -19,8 +17,7 @@ public class Robot {
     public static Robot init(HardwareMap hardwareMap){
         Robot robot = new Robot();
         drivetrain.init(hardwareMap);
-        spinningBrushIntake.init(hardwareMap);
-        servo.init(hardwareMap);
+        //servo.init(hardwareMap);
         return robot;
     }
 }
