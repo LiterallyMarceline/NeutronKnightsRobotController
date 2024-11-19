4941a315c31902e98fa6dca0dd93983b76ba27fb
MasonCenterforRoboti <aditi.lone@masonrobotics.org>
2024-11-18 20:19:03 -0500

    Skeleton Vision

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/mechanisms/Vision.java
@@ -0,0 +1,18 @@
+package org.firstinspires.ftc.neutronknightscode.main.robot.mechanisms;
+
+public class Vision {
+
+    double xPosition;
+    double yPosition;
+
+    public double getPlacement()
+    {
+        return xPosition;
+
+
+    }
+    public double calculateLocation()
+    {
+        return yPosition;
+    }
+}
