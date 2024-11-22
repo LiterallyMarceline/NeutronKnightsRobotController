246cbe112cd9adb85fba7591f0377a4e14997cc3
= <=>
2024-11-21 20:00:31 -0500

    X and Y positions added to Drivetrain and Field

--- TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/mechanisms/Vision.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/neutronknightscode/main/robot/mechanisms/Vision.java
@@ -2,10 +2,12 @@ package org.firstinspires.ftc.neutronknightscode.main.robot.mechanisms;
 
 import android.graphics.Point;
 
+import org.firstinspires.ftc.neutronknightscode.main.field.Field;
+
 public class Vision {
 
     public Point calculateLocation() {
-        return new Point(0,0);
+        if(!Field.checkInbounds(new Point(0,0))) throw new Error("Out of Bounds"); else return new Point(0,0);
     }
     public RobotPlacements getPlacement() {
         return RobotPlacements.RED_LEFT;
